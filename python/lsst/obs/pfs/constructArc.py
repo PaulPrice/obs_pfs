cf347e2194ea25c349dcdbcfde31c8536f60363d
Andreas Ritter <azuri.ritter@gmail.com>
2016-10-20 23:57:46 -0400

    Add ConstructArcTask

---
+++ python/lsst/obs/pfs/constructArc.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import lsst.afw.detection as afwDet
+#import lsst.afw.image as afwImage
+#from lsst.ctrl.pool.pool import NODE
+import lsst.meas.algorithms as measAlg
+from lsst.pex.config import Field, ConfigurableField
+from lsst.pipe.drivers.constructCalibs import CalibConfig, CalibTask
+from lsst.pipe.tasks.repair import RepairTask
+#from lsst.pipe.drivers.utils import getDataRef
+import math
+#import numpy as np
+#from pfs.datamodel.pfsFiberTrace import PfsFiberTrace
+#import pfs.drp.stella.createFlatFiberTraceProfileTask as cfftpTask
+#import pfs.drp.stella.findAndTraceAperturesTask as fataTask
+
+class ConstructArcConfig(CalibConfig):
+    """Configuration for Arc construction"""
+    doRepair = Field(dtype=bool, default=True, doc="Repair artifacts?")
+    psfFwhm = Field(dtype=float, default=3.0, doc="Repair PSF FWHM (pixels)")
+    psfSize = Field(dtype=int, default=21, doc="Repair PSF size (pixels)")
+    crGrow = Field(dtype=int, default=2, doc="Grow radius for CR (pixels)")
+    repair = ConfigurableField(target=RepairTask, doc="Task to repair artifacts")
+
+class ConstructArcTask(CalibTask):
+    """Task to construct the Arc"""
+    ConfigClass = ConstructArcConfig
+    _DefaultName = "constructArc"
+    calibName = "arc"
+
+    def __init__(self, *args, **kwargs):
+        CalibTask.__init__(self, *args, **kwargs)
+        self.makeSubtask("repair")
+
+    @classmethod
+    def applyOverrides(cls, config):
+        """Overrides to apply for Arc construction"""
+        config.isr.doFringe = False
+
+    def processSingle(self, sensorRef):
+        """Process a single CCD
+
+        Besides the regular ISR, also masks cosmic-rays.
+        """
+        
+        exposure = CalibTask.processSingle(self, sensorRef)
+
+        if self.config.doRepair:
+            psf = measAlg.DoubleGaussianPsf(self.config.psfSize, self.config.psfSize,
+                                            self.config.psfFwhm/(2*math.sqrt(2*math.log(2))))
+            exposure.setPsf(psf)
+            self.repair.run(exposure, keepCRs=False)
+            if self.config.crGrow > 0:
+                mask = exposure.getMaskedImage().getMask().clone()
+                mask &= mask.getPlaneBitMask("CR")
+                fpSet = afwDet.FootprintSet(mask.convertU(), afwDet.Threshold(0.5))
+                fpSet = afwDet.FootprintSet(fpSet, self.config.crGrow, True)
+                fpSet.setMask(exposure.getMaskedImage().getMask(), "CR")
+        return exposure
